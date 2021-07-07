d49e7dab79404cdd56638cc5e2d0485417d04cb1
juvenalbruno <bruno.andrade178@hotmail.com>
2021-07-07 19:10:56 -0300

    20210707_1910

--- web/src/pages/Modules.tsx
+++ web/src/pages/Modules.tsx
@@ -1,8 +1,25 @@
+import { Button } from '../components/Button';
+
+import LogoIMG from '../assets/img/logo.png';
+
+import '../styles/modules.scss';
 
 export function Modules() {
     return (
-        <>
-            <h1>Modules</h1>
-        </>
+        <div id="page-modules">
+            <header>
+                <div className="content">
+                    <img src={LogoIMG} alt="Verzel" />
+                    <Button>Entrar</Button>
+                </div>
+            </header>
+
+            <main>
+                <div className="modules-title">
+                    <h1>Módulos</h1>
+                    <p>Selecione o módulo para ver as aulas disponíveis:</p>
+                </div>
+            </main>
+        </div>
     );
 }
