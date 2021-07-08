9175d97932bd86c91c372ed106c9ae30a1b172c4
juvenalbruno <bruno.andrade178@hotmail.com>
2021-07-07 23:50:59 -0300

    20210707_2350

--- web/src/pages/Modules.tsx
+++ web/src/pages/Modules.tsx
@@ -28,7 +28,7 @@ export function Modules() {
                         </div>
                         <div className="modules-info">
                             <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
+                            <p>3 aulas</p>
                         </div>
                     </div>
                     <div className="modules-wrapper">
@@ -36,8 +36,8 @@ export function Modules() {
                             <FaConnectdevelop />
                         </div>
                         <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
+                            <h1>Introdução a ReactJS</h1>
+                            <p>15 aulas</p>
                         </div>
                     </div>
                     <div className="modules-wrapper">
@@ -45,8 +45,8 @@ export function Modules() {
                             <FaConnectdevelop />
                         </div>
                         <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
+                            <h1>Introdução NodeJS</h1>
+                            <p>33 aulas</p>
                         </div>
                     </div>
                     <div className="modules-wrapper">
@@ -54,8 +54,8 @@ export function Modules() {
                             <FaConnectdevelop />
                         </div>
                         <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
+                            <h1>Introdução JavaScript</h1>
+                            <p>55 aulas</p>
                         </div>
                     </div>
                     <div className="modules-wrapper">
@@ -63,29 +63,58 @@ export function Modules() {
                             <FaConnectdevelop />
                         </div>
                         <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
-                        </div>
-                    </div>
-                    <div className="modules-wrapper">
-                        <div className="modules-icon">
-                            <FaConnectdevelop />
-                        </div>
-                        <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
-                        </div>
-                    </div>
-                    <div className="modules-wrapper">
-                        <div className="modules-icon">
-                            <FaConnectdevelop />
-                        </div>
-                        <div className="modules-info">
-                            <h1>Introdução e Preparatório</h1>
-                            <p>3/3 aulas</p>
+                            <h1>Introdução typeScript</h1>
+                            <p>5 aulas</p>
                         </div>
                     </div>
                 </div>
+           
+                <div className="aulas-title">
+                    <div className="aulas-icon">
+                        <FaConnectdevelop />
+                    </div>
+                    <div>
+                        <h1>Lógica de Programação</h1>
+                        <p>Todas as aulas dísponiveis nesse módulo:</p>
+                    </div>
+                </div>
+
+                <div className="aulas-content">
+                    <div className="aulas-wrapper">
+                        <div className="aulas-info">
+                            <img src="https://miro.medium.com/max/3840/1*vHHBwcUFUaHWXntSnqKdCA.png" alt="" />
+
+                            <h1>Introdução a ReactJS</h1>
+
+                            <p>Vamos criar uma aplicação web utilizando o React juntamento com o TypeScript e o nodeJS.</p>
+                        </div>
+
+                        <Button>Assistir aula</Button>
+                    </div>
+                    <div className="aulas-wrapper">
+                        <div className="aulas-info">
+                            <img src="https://miro.medium.com/max/3840/1*vHHBwcUFUaHWXntSnqKdCA.png" alt="" />
+
+                            <h1>Introdução a ReactJS</h1>
+
+                            <p>Vamos criar uma aplicação web utilizando o React juntamento com o TypeScript e o nodeJS.</p>
+                        </div>
+
+                        <Button>Assistir aula</Button>
+                    </div>
+                    <div className="aulas-wrapper">
+                        <div className="aulas-info">
+                            <img src="https://miro.medium.com/max/3840/1*vHHBwcUFUaHWXntSnqKdCA.png" alt="" />
+
+                            <h1>Introdução a ReactJS</h1>
+
+                            <p>Vamos criar uma aplicação web utilizando o React juntamento com o TypeScript e o nodeJS.</p>
+                        </div>
+
+                        <Button>Assistir aula</Button>
+                    </div>
+                </div>
+
             </main>
         </div>
     );
